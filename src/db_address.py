6feac13cbdd75bcd6183e8061ebaef9725d74fce
soldy <4786022+soldy@users.noreply.github.com>
2024-04-18 19:50:44 +0100

    address db refactoring
    Signed-off-by: soldy <4786022+soldy@users.noreply.github.com>

--- src/db_address.py
+++ src/db_address.py
@@ -1,300 +1,312 @@
 import sqlite3
-import mailsort.db_tools
+import mailsort.db_tools as tools
+import mailsort.db_conf as conf
 
-
-def lookDomainTop(name_: str):
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
-   )
-   cur = con.cursor()
-   cur.execute(
+def lookByName(name_:str, table_:str ):
+   return tools.fetchByString(
+     name_,
      """
        SELECT * FROM 
-         domain_top 
+         {0} 
        WHERE 
          name = ?
      """, 
-     (name_,)
+     [
+       table_
+     ],
+     conf.file_address
    )
-   data = cur.fetchall()
-   con.close()
-   return data
+
+def addWithName(
+  name_:str,
+  table_:list[str]
+)->int:
+  tools.lastWord(
+    (name_,),
+    """
+      INSERT INTO {0}
+        (name)
+      VALUES (?);
+    """,
+    [
+      table_
+    ],
+    conf.file_address
+  )
+
+def lookDomainTop(name_: str):
+   return lookByName(
+     name_,
+     conf.table_domain_top
+   )
 
 def addDomainTop(name_: str)->int:
    data = lookDomainTop(name_)
    if len(data) > 0:
-      return data[0][0]
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
+     return data[0][0]
+   return addWithName(
+     name_,
+     conf.table_domain_top
    )
-   cur = con.cursor()
-   cur.execute(
-     """
-       INSERT INTO domain_top
-         (name)
-       VALUES (?);
-     """,
-     (name_,)
-   )
-   con.commit()
-   con.close()
-   data = lookDomainTop(name_)
-   return data[0][0]
 
 def lookDomainSecond(name_: str):
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
+   return lookByName(
+     name_,
+     conf.table_domain_second
    )
-   cur = con.cursor()
-   cur.execute(
-     """
-       SELECT * FROM 
-         domain_second 
-       WHERE 
-         name = ?
-     """, 
-     (name_,)
-   )
-   data = cur.fetchall()
-   con.close()
-   return data
 
 def addDomainSecond(name_: str)->int:
    data = lookDomainSecond(name_)
    if len(data) > 0:
-      return data[0][0]
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
+     return data[0][0]
+   return addWithName(
+     name_,
+     conf.table_domain_second
    )
-   cur = con.cursor()
-   cur.execute(
-     """
-       INSERT INTO domain_second
-         (name)
-       VALUES (?);
-     """,
-     (name_,)
-   )
-   con.commit()
-   con.close()
-   data = lookDomainSecond(name_)
-   return data[0][0]
 
 def lookDomainSub(name_: str):
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
+   return lookByName(
+     name_,
+     conf.table_domain_sub
    )
-   cur = con.cursor()
-   cur.execute(
-     """
-       SELECT * FROM 
-         domain_sub 
-       WHERE 
-         name = ?
-     """, 
-     (name_,)
-   )
-   data = cur.fetchall()
-   con.close()
-   return data
 
 def addDomainSub(name_: str)->int:
    data = lookDomainSub(name_)
    if len(data) > 0:
-      return data[0][0]
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
+     return data[0][0]
+   return addWithName(
+     name_,
+     conf.table_domain_sub
    )
-   cur = con.cursor()
-   cur.execute(
-     """
-       INSERT INTO domain_sub
-         (name)
-       VALUES (?);
-     """,
-     (name_,)
-   )
-   con.commit()
-   con.close()
-   data = lookDomainSub(name_)
-   return data[0][0]
 
 def lookAlias(name_: str):
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
+   return lookByName(
+     name_,
+     conf.table_alias
    )
-   cur = con.cursor()
-   cur.execute(
-     """
-       SELECT * FROM 
-         alias 
-       WHERE 
-         name = ?
-     """, 
-     (name_,)
-   )
-   data = cur.fetchall()
-   con.close()
-   return data
 
 def addAlias(name_: str)->int:
    data = lookAlias(name_)
    if len(data) > 0:
-      return data[0][0]
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
+     return data[0][0]
+   return addWithName(
+     name_,
+     conf.table_alias
    )
-   cur = con.cursor()
-   cur.execute(
-     """
-       INSERT INTO alias
-         (name)
-       VALUES (?);
-     """,
-     (name_,)
-   )
-   con.commit()
-   con.close()
-   data = lookAlias(name_)
-   return data[0][0]
 
-def lookDomain(top_: int, second_:int, sub:int):
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
-   )
-   cur = con.cursor()
-   cur.execute(
-     """
-       SELECT * FROM 
-         domain 
-       WHERE 
-         top = ?
-       AND
-         second = ?
-       AND
-         sub = ?
-     """, 
-     (top_,second_,sub_)
-   )
-   data = cur.fetchall()
-   con.close()
-   return data
+def lookDomain(top_: int, second_:int, sub_:int):
+  return tools.fetchAll(
+    (top_,second_,sub_),
+    """
+      SELECT * FROM 
+        {0}
+      WHERE 
+        top = ?
+      AND
+        second = ?
+      AND
+        sub = ?
+    """, 
+    [
+      conf.table_domain
+    ],
+    conf.file_address
+  )
 
-def addDomain(top_: int, second_:int, sub:int)->int:
-   data = lookAlias(name_)
-   if len(data) > 0:
-      return data[0][0]
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
-   )
-   cur = con.cursor()
-   cur.execute(
-     """
-       INSERT INTO domain
-         (top, second, sub)
-       VALUES (?, ?, ?);
-     """,
-     (top_,second_,sub_)
-   )
-   con.commit()
-   con.close()
-   data = lookAlias(name_)
-   return data[0][0]
+def addDomain(top_: int, second_:int, sub_:int)->int:
+  data = lookDomain(top_, second_, sub_)
+  if len(data) > 0:
+     return data[0][0]
+  return tools.lastWord(
+    (top_,second_,sub_),
+    """
+      INSERT INTO domain
+        (top, second, sub)
+      VALUES (?, ?, ?);
+    """,
+    [
+      conf.table_domain
+    ],
+    conf.file_address
+  )
 
-def lookEmail(top_: int, second_:int, sub:int):
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
-   )
-   cur = con.cursor()
-   cur.execute(
-     """
-       SELECT * FROM 
-         email_address 
-       WHERE 
-         domain = ?
-       AND
-         alias = ?
-     """, 
-     (domain, alias)
-   )
-   data = cur.fetchall()
-   con.close()
-   return data
+def lookEmail(alias_: int, domain_:int)->int:
+  return tools.fetchAll(
+    (domain_, alias_),
+    """
+      SELECT * FROM 
+        {0}
+      WHERE 
+        domain = ?
+      AND
+        alias = ?
+    """, 
+    [
+      conf.table_email_address
+    ],
+    conf.file_address
+  )
 
-def addEmail(top_: int, second_:int, sub:int)->int:
-   data = lookAlias(name_)
-   if len(data) > 0:
-      return data[0][0]
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
-   )
-   cur = con.cursor()
-   cur.execute(
-     """
-       INSERT INTO email_address
-         (domain, alias)
-       VALUES (?, ?);
-     """,
-     (domain, alias)
-   )
-   con.commit()
-   con.close()
-   data = lookAlias(name_)
-   return data[0][0]
+def addEmail(alias_: int, domain_:int)->int:
+  data = lookEmail(alias_, domain_)
+  if len(data) > 0:
+     return data[0][0]
+  return tools.lastWord(
+    (domain_, alias_),
+    """
+      INSERT INTO {0}
+        (domain, alias)
+      VALUES (?, ?);
+    """,
+    [
+      conf.table_email_address
+    ],
+    conf.file_address
+  )
 
 def getDomain(id_:int):
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
-   )
-   cur = con.cursor()
-   cur.execute(
-     """
-       SELECT domain.id, domain_sub.name, domain_second.name, domain_top.name FROM 
-         domain
-       LEFT JOIN domain_sub
-       ON domain.sub = domain_sub.id
-       LEFT JOIN domain_second
-       ON domain.second = domain_second.id
-       LEFT JOIN domain_top
-       ON domain.top = domain_top.id
-       WHERE 
-         domain.id = ?
-     """, 
-     (id_,)
-   )
-   data = cur.fetchall()
-   con.close()
-   return data
+  return tools.fetchById(
+    id_,
+    """
+      SELECT 
+        {0}.id,
+        {1}.name,
+        {2}.name,
+        {3}.name 
+      FROM {0}
+      LEFT JOIN {1}
+      ON {0}.sub = {1}.id
+      LEFT JOIN {2}
+      ON {0}.second = {2}.id
+      LEFT JOIN {3}
+      ON {0}.top = {3}.id
+      WHERE 
+        {0}.id = ?
+    """, 
+    [
+      conf.table_domain,
+      conf.table_domain_sub,
+      conf.table_domain_second,
+      conf.table_domain_top,
+    ],
+    conf.file_address
+  )
 
 def getEmail(id_:int):
-   con = sqlite3.connect(
-     mailsort.db_conf._file_address
+  return tools.fetchById(
+    id_,
+    """
+      SELECT 
+        {0}.id,
+        {1}.name,
+        {3}.name,
+        {4}.name,
+        {5}.name
+      FROM {0}
+      LEFT JOIN {1}
+      ON {0}.alias = {1}.id
+      LEFT JOIN {2}
+      ON {0}.domain = {2}.id
+      LEFT JOIN {3}
+      ON {2}.sub = {3}.id
+      LEFT JOIN {4}
+      ON {2}.second = {4}.id
+      LEFT JOIN {5}
+      ON {2}.top = {5}.id
+      WHERE 
+        {0}.id = ?
+    """, 
+    [
+      conf.table_email_address,
+      conf.table_alias,
+      conf.table_domain,
+      conf.table_domain_sub,
+      conf.table_domain_second,
+      conf.table_domain_top,
+    ],
+    conf.file_address
+  )
+
+def lookNamePart(name_: str):
+   return lookByName(
+     name_,
+     conf.table_name_part
    )
-   cur = con.cursor()
-   cur.execute(
-     """
-       SELECT 
-         email_address.id,
-         alias.name,
-         domain_sub.name,
-         domain_second.name,
-         domain_top.name
-       FROM email_address
-       LEFT JOIN alias
-       ON email_address.alias = alias.id
-       LEFT JOIN domain
-       ON email_address.domain = domain.id
-       LEFT JOIN domain_sub
-       ON domain.sub = domain_sub.id
-       LEFT JOIN domain_second
-       ON domain.second = domain_second.id
-       LEFT JOIN domain_top
-       ON domain.top = domain_top.id
-       WHERE 
-         email_address.id = ?
-     """, 
-     (id_,)
+
+def addNamePart(name_: str)->int:
+   data = lookNamePart(name_)
+   if len(data) > 0:
+     return data[0][0]
+   return addWithName(
+     name_,
+     conf.table_name_part
    )
-   data = cur.fetchall()
-   con.close()
-   return data
+
+def addName()->int:
+  return tools.lastWord(
+    (),
+    """
+      INSERT INTO {0}
+      (id) VALUES (NULL);
+    """,
+    [
+      conf.table_name
+    ],
+    conf.file_address
+  )
+
+def lookNameFull(name_:int, part_:int)->bool:
+  return tools.fetchAll(
+    (name_, part_),
+    """
+      SELECT * FROM {0}
+      WHERE 
+        name = ?
+      AND 
+        part = ?
+    """,
+    [
+      conf.table_name_full
+    ],
+    conf.file_address
+  )
+
+
+def addNameFull(name_:int, part_:int):
+  data = lookNameFull(name_, part_)
+  if len(data) > 0:
+     return data[0][0]
+  tools.execute(
+    (name_, part_),
+    """
+      INSERT INTO {0}
+      (name,part) VALUES (?,?);
+    """,
+    [
+      conf.table_name_full
+    ],
+    conf.file_address
+  )
+
+def getNameFull(id_:int)->tuple:
+  return tools.fetchById(
+    id_,
+    """
+      SELECT 
+        {0}.id,
+        {2}.name
+      FROM {0}
+      LEFT JOIN {1}
+      ON {0}.id = {1}.name
+      LEFT JOIN {2}
+      ON {1}.part = {2}.id
+      WHERE 
+        {0}.id = ?
+    """, 
+    [
+      conf.table_name,
+      conf.table_name_full,
+      conf.table_name_part
+    ],
+    conf.file_address
+  )
