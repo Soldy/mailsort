99756c5c3cc9027c406e5dfe4c56ac49cc931936
soldy <4786022+soldy@users.noreply.github.com>
2024-04-19 19:32:03 +0100

    address to more dry
    Signed-off-by: soldy <4786022+soldy@users.noreply.github.com>

--- src/db_address.py
+++ src/db_address.py
@@ -2,18 +2,13 @@ import sqlite3
 import mailsort.db_tools as tools
 import mailsort.db_conf as conf
 
+
+tr = tools.Transitor(conf.file_address)
+
 def lookByName(name_:str, table_:str ):
-   return tools.fetchByString(
+   return tools.lookByName(
      name_,
-     """
-       SELECT * FROM 
-         {0} 
-       WHERE 
-         name = ?
-     """, 
-     [
-       table_
-     ],
+     table_,
      conf.file_address
    )
 
@@ -21,21 +16,14 @@ def addWithName(
   name_:str,
   table_:list[str]
 )->int:
-  tools.lastWord(
-    (name_,),
-    """
-      INSERT INTO {0}
-        (name)
-      VALUES (?);
-    """,
-    [
-      table_
-    ],
+  return tools.addWithName(
+    name_,
+    table_,
     conf.file_address
   )
 
 def lookDomainTop(name_: str):
-   return lookByName(
+   return tr.lookByName(
      name_,
      conf.table_domain_top
    )
@@ -44,13 +32,13 @@ def addDomainTop(name_: str)->int:
    data = lookDomainTop(name_)
    if len(data) > 0:
      return data[0][0]
-   return addWithName(
+   return tr.addWithName(
      name_,
      conf.table_domain_top
    )
 
 def lookDomainSecond(name_: str):
-   return lookByName(
+   return tr.lookByName(
      name_,
      conf.table_domain_second
    )
@@ -59,13 +47,13 @@ def addDomainSecond(name_: str)->int:
    data = lookDomainSecond(name_)
    if len(data) > 0:
      return data[0][0]
-   return addWithName(
+   return tr.addWithName(
      name_,
      conf.table_domain_second
    )
 
 def lookDomainSub(name_: str):
-   return lookByName(
+   return tr.lookByName(
      name_,
      conf.table_domain_sub
    )
@@ -74,13 +62,13 @@ def addDomainSub(name_: str)->int:
    data = lookDomainSub(name_)
    if len(data) > 0:
      return data[0][0]
-   return addWithName(
+   return tr.addWithName(
      name_,
      conf.table_domain_sub
    )
 
 def lookAlias(name_: str):
-   return lookByName(
+   return tr.lookByName(
      name_,
      conf.table_alias
    )
@@ -89,13 +77,13 @@ def addAlias(name_: str)->int:
    data = lookAlias(name_)
    if len(data) > 0:
      return data[0][0]
-   return addWithName(
+   return tr.addWithName(
      name_,
      conf.table_alias
    )
 
 def lookDomain(top_: int, second_:int, sub_:int):
-  return tools.fetchAll(
+  return tr.fetchAll(
     (top_,second_,sub_),
     """
       SELECT * FROM 
@@ -109,15 +97,14 @@ def lookDomain(top_: int, second_:int, sub_:int):
     """, 
     [
       conf.table_domain
-    ],
-    conf.file_address
+    ]
   )
 
 def addDomain(top_: int, second_:int, sub_:int)->int:
   data = lookDomain(top_, second_, sub_)
   if len(data) > 0:
      return data[0][0]
-  return tools.lastWord(
+  return tr.lastWord(
     (top_,second_,sub_),
     """
       INSERT INTO domain
@@ -126,46 +113,11 @@ def addDomain(top_: int, second_:int, sub_:int)->int:
     """,
     [
       conf.table_domain
-    ],
-    conf.file_address
-  )
-
-def lookEmail(alias_: int, domain_:int)->int:
-  return tools.fetchAll(
-    (domain_, alias_),
-    """
-      SELECT * FROM 
-        {0}
-      WHERE 
-        domain = ?
-      AND
-        alias = ?
-    """, 
-    [
-      conf.table_email_address
-    ],
-    conf.file_address
-  )
-
-def addEmail(alias_: int, domain_:int)->int:
-  data = lookEmail(alias_, domain_)
-  if len(data) > 0:
-     return data[0][0]
-  return tools.lastWord(
-    (domain_, alias_),
-    """
-      INSERT INTO {0}
-        (domain, alias)
-      VALUES (?, ?);
-    """,
-    [
-      conf.table_email_address
-    ],
-    conf.file_address
+    ]
   )
 
 def getDomain(id_:int):
-  return tools.fetchById(
+  return tr.fetchById(
     id_,
     """
       SELECT 
@@ -188,12 +140,44 @@ def getDomain(id_:int):
       conf.table_domain_sub,
       conf.table_domain_second,
       conf.table_domain_top,
-    ],
-    conf.file_address
+    ]
   )
 
+def lookEmail(alias_: int, domain_:int)->int:
+  return tr.fetchAll(
+    (domain_, alias_),
+    """
+      SELECT * FROM 
+        {0}
+      WHERE 
+        domain = ?
+      AND
+        alias = ?
+    """, 
+    [
+      conf.table_email_address
+    ]
+  )
+
+def addEmail(alias_: int, domain_:int)->int:
+  data = lookEmail(alias_, domain_)
+  if len(data) > 0:
+     return data[0][0]
+  return tr.lastWord(
+    (domain_, alias_),
+    """
+      INSERT INTO {0}
+        (domain, alias)
+      VALUES (?, ?);
+    """,
+    [
+      conf.table_email_address
+    ]
+  )
+
+
 def getEmail(id_:int):
-  return tools.fetchById(
+  return tr.fetchById(
     id_,
     """
       SELECT 
@@ -223,12 +207,11 @@ def getEmail(id_:int):
       conf.table_domain_sub,
       conf.table_domain_second,
       conf.table_domain_top,
-    ],
-    conf.file_address
+    ]
   )
 
 def lookNamePart(name_: str):
-   return lookByName(
+   return tr.lookByName(
      name_,
      conf.table_name_part
    )
@@ -237,13 +220,13 @@ def addNamePart(name_: str)->int:
    data = lookNamePart(name_)
    if len(data) > 0:
      return data[0][0]
-   return addWithName(
+   return tr.addWithName(
      name_,
      conf.table_name_part
    )
 
 def addName()->int:
-  return tools.lastWord(
+  return tr.lastWord(
     (),
     """
       INSERT INTO {0}
@@ -251,62 +234,138 @@ def addName()->int:
     """,
     [
       conf.table_name
-    ],
-    conf.file_address
+    ]
   )
 
-def lookNameFull(name_:int, part_:int)->bool:
-  return tools.fetchAll(
-    (name_, part_),
+def lookNameFull(name_:int, part_:int, serial_:int)->bool:
+  return tr.fetchAll(
+    (name_, part_, serial_),
     """
       SELECT * FROM {0}
       WHERE 
         name = ?
       AND 
         part = ?
+      AND 
+        serial = ?
     """,
     [
       conf.table_name_full
-    ],
-    conf.file_address
+    ]
   )
 
 
-def addNameFull(name_:int, part_:int):
-  data = lookNameFull(name_, part_)
+def addNameFull(name_:int, part_:int, serial_:int):
+  data = lookNameFull(name_, part_, serial_)
   if len(data) > 0:
-     return data[0][0]
-  tools.execute(
-    (name_, part_),
+     return
+  tr.execute(
+    (name_, part_, serial_),
     """
       INSERT INTO {0}
-      (name,part) VALUES (?,?);
+      (name,part,serial) VALUES (?,?,?);
     """,
     [
       conf.table_name_full
-    ],
-    conf.file_address
+    ]
   )
 
 def getNameFull(id_:int)->tuple:
-  return tools.fetchById(
+  return tr.fetchById(
     id_,
     """
-      SELECT 
+      SELECT
         {0}.id,
-        {2}.name
+        GROUP_CONCAT({2}.name, " ") 
       FROM {0}
       LEFT JOIN {1}
-      ON {0}.id = {1}.name
+      ON {1}.name = {0}.id
       LEFT JOIN {2}
       ON {1}.part = {2}.id
-      WHERE 
-        {0}.id = ?
+      WHERE {1}.name = ?
+      ORDER BY {1}.serial ASC
     """, 
     [
       conf.table_name,
       conf.table_name_full,
       conf.table_name_part
-    ],
-    conf.file_address
+    ]
+  )
+
+def lookAddress(name_:int, email_:int):
+  return tr.fetchAll(
+    (name_, email_),
+    """
+      SELECT * FROM {0}
+      WHERE 
+        name = ?
+      AND 
+        email = ?
+    """,
+    [
+      conf.table_address
+    ]
+  )
+
+
+def addAddress(name_:int, email_:int)->int:
+  data = lookAddress(name_, email_)
+  if len(data) > 0:
+     return data[0][0]
+  return tr.lastWord(
+    (name_, email_),
+    """
+      INSERT INTO {0}
+      (name,email) VALUES (?,?);
+    """,
+    [
+      conf.table_address
+    ]
+  )
+
+def getAddress(id_:int):
+  return tr.fetchById(
+    id_,
+    """
+      SELECT
+        {0}.id,
+        (
+         SELECT
+          GROUP_CONCAT({2}.name, " ") 
+          FROM {1}
+          LEFT JOIN {2}
+          ON {1}.part = {2}.id
+          WHERE {1}.name = {0}.name
+          ORDER BY {1}.serial ASC
+        ),
+        {4}.name,
+        {6}.name,
+        {7}.name,
+        {8}.name
+      FROM {0}
+      LEFT JOIN {3}
+      ON {0}.email = {3}.id
+      LEFT JOIN {4}
+      ON {3}.alias = {4}.id
+      LEFT JOIN {5}
+      ON {3}.domain = {5}.id
+      LEFT JOIN {6}
+      ON {5}.sub = {6}.id
+      LEFT JOIN {7}
+      ON {5}.second = {7}.id
+      LEFT JOIN {8}
+      ON {5}.top = {8}.id
+      WHERE {0}.id = ?
+    """, 
+    [
+      conf.table_address,
+      conf.table_name_full,
+      conf.table_name_part,
+      conf.table_email_address,
+      conf.table_alias,
+      conf.table_domain,
+      conf.table_domain_sub,
+      conf.table_domain_second,
+      conf.table_domain_top,
+    ]
   )
